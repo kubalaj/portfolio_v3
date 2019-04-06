7f0b3cb06dbf5e38dd2ad6054fc32ac9ab2b101f
kubalaj <jp@jpkubala.com>
2019-04-06 09:57:09 -0700

    starting to move content around

--- pages/index.js
+++ pages/index.js
@@ -5,33 +5,45 @@ import "../styles.scss";
 
 export default () => (
   <div>
-    <div className="welcome">
-      <h1>J Paul Kubala</h1>
-      <h2 id="creative">creative.</h2>
-      <h2 id="technolighist">technologist.</h2>
-      <h2 id="story-teller">story-teller.</h2>
-    </div>
     <Layout>
       <div className="content">
-        <div className="gutter-33">
-          <Brick title="Piano" source="/static/piano/piano.png" link="/piano" />
-          <Brick
-            title="The Oblong Adventure"
-            source="/static/the_oblong_adventure/detail.png"
-            link="/oblongadventure"
-          />
-          <Brick
-            title="openGL Pacman P.O.C."
-            source="/static/pacman/hero.png"
-            link="/pacman"
-          />
+        <div className="row">
+          <div className="gutter-50">
+            <Brick
+              title="Piano"
+              source="/static/piano/piano.png"
+              link="/piano"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="gutter-50">
+            <Brick
+              title="The Oblong Adventure"
+              source="/static/the_oblong_adventure/detail.png"
+              link="/oblongadventure"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="gutter-50">
+            <Brick
+              title="openGL Pacman P.O.C."
+              source="/static/pacman/hero.png"
+              link="/pacman"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="gutter-50">
+            <Brick
+              title="Tic Tac Toe Game"
+              source="/static/tic_tac_toe/hero.png"
+              link="/tictactoe"
+            />
+          </div>
         </div>
         <div className="gutter-33">
-          <Brick
-            title="Tic Tac Toe Game"
-            source="/static/tic_tac_toe/hero.png"
-            link="/tictactoe"
-          />
           <Brick
             title="The Mess 16mm Film"
             source="/static/the_mess/theMess.gif"
